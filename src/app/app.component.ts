f592309d9a44e0960c102694d7802079f41d2c53
Edward <nedinsuyun@gmail.com>
2021-11-04 15:40:24 -0400

    fix readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -97,19 +97,3 @@ export class AppComponent {
 export class UserDialog {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
 }
-
-// Assignment UI Developer:
-// Create an angular application
-// Features:
-// 1. Dashboard loading multiple cards
-// a. UserInfo card
-// b. All the existing users list (clickable)
-// 2. On click of the existing user in the (1.b. All the users) load the selected user info data in the 1.a.
-// UserInfo card
-// 3. Multiple Login
-// a. Admin (All cards visible)
-// b. DemoUser (Only UserInfo card visible)
-// 4. Create a fake server and make httpget and httppost calls to load data in the cards.
-
-// Note:
-// 1. Create a new repo in any of the public git repos and please share the url.
